96b94a131bbc7290d0a32fb4cfc74f89553aa385
Jan Bolmer <jan@bolmer.de>
2014-06-08 00:50:43 +0200

    get_ebv.py added
    get galactic foreground reddening from S&F an Schlegel et. al

---
+++ get_ebv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import requests
+from lxml import html
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-c", "--coords", dest="coordinates", nargs=2)
+args = parser.parse_args()
+coords = args.coordinates[0] + args.coordinates[1]
+
+def getebv(coords):
+
+	url = "http://irsa.ipac.caltech.edu/cgi-bin/DUST/nph-dust?locstr=" + coords
+	page = requests.get(url)
+	tree = html.fromstring(page.text)
+	
+	ebv_schlafly = tree.xpath('//meanvaluesandf/text()')
+	ebv_schlegel = tree.xpath('//meanvaluesfd/text()')
+	
+	split_schlafly = ebv_schlafly[0].split()
+	split_schlegel = ebv_schlegel[0].split()
+	
+	schlafly_corr = float(split_schlafly[0])
+	schlegel_corr = float(split_schlegel[0])
+
+	print "Schlafly & Finkbeiner 2011; Mean Value:", schlafly_corr, "(mag)"
+	print "Schlegel et al. 1998; Mean Value:", schlegel_corr, "(mag)"
+
+	return schlafly_corr, schlegel_corr
+
+if __name__ == "__main__":
+	getebv(coords)
